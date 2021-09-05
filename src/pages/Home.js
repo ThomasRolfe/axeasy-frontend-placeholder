4af67c768455bea231106339fe592f77e2cfdf5f
Tom Rolfe <thomasrolfe@yahoo.co.uk>
2021-09-05 22:26:33 +0100

    Axios setup and convertkit API request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,21 +2,42 @@ import HeroImage from "../img/axeasy-scholarship-overview.png";
 import {AxeasyLogo} from "../svg/AxeasyLogo";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import axios from "axios";
+import { useState } from "react";
+
+const formId = 2574758;
+
 
 export default function Home() {
+    const [email, setEmail] = useState('');
+
+    const submitSignupForm = (e) => {
+        e.preventDefault();
+        axios.post(`https://api.convertkit.com/v3/forms/${formId}/subscribe`, {
+            api_key: process.env.REACT_APP_CONVERTKIT_API_KEY,
+            email: email
+        })
+        .then(response => {
+            console.log(response)
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+    }
+
     return (
         <>
-        <nav className="flex items-center justify-between mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-8xl min-h-10 py-6">
+        <nav className="flex items-center justify-between mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-8xl min-h-10 py-6 ">
             <div>                
                 <AxeasyLogo  className="h-10 md:h-14 " />
             </div>
-            <div>
+            <div className="z-20">
                 <a href="https://twitter.com/home" rel="noreferrer" target="_blank" className="" title="Get in touch with us on Twitter">
-                <FontAwesomeIcon size="2x" className="text-twitter-blue" icon={faTwitter} />
+                    <FontAwesomeIcon className="text-twitter-blue  text-4xl hover:scale-125 transform transition" icon={faTwitter} />
                 </a>
             </div>
         </nav>
-        <div className="bg-white pb-8 sm:pb-12 lg:pb-12 -mt-10 md:-mt-14">
+        <div className="bg-white pb-8 sm:pb-12 lg:pb-12 -mt-10 md:-mt-14 z-0">
             <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
                 <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-8xl lg:grid lg:grid-cols-2 lg:gap-18">
                     <div>
@@ -34,6 +55,7 @@ export default function Home() {
                             <form
                                 action="#"
                                 className="mt-12 sm:max-w-lg sm:w-full sm:flex"
+                                onSubmit={submitSignupForm}
                             >
                                 <div className="min-w-0 flex-1">
                                     <label
@@ -47,6 +69,9 @@ export default function Home() {
                                         type="email"
                                         className="block w-full bg-gray-100 border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 focus:border-brand-light-turq focus:ring-brand-light-turq font-muli"
                                         placeholder="Enter your email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
                                     />
                                 </div>
                                 <div className="mt-4 sm:mt-0 sm:ml-3">
